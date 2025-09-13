c90fef7d4c03ba90df4198f3002d9e120e237d5a
tawfiq erassy <tawfiqerassy4@gmail.com>
2025-09-13 22:00:04 +0300

    Initial commit - Visit Mwanza Website

--- script.js
+++
@@ -1,5 +0,0 @@
-function showsection(sectionid) {
-    const sections = document.querySelectorAll('section');
-    sections.forEach(sec => sec.style.display = 'none');
-    document.getElementById(sectionid).style.display = 'block';
-}
